e2cf9f41421295681ccdb4921a12c48f8785587f
RaminIsayevDev <ramin.isayev.dev@gmail.com>
2025-07-15 23:56:11 +0400

    update

--- example1/build/esp-idf/espcoredump/CMakeFiles/__idf_espcoredump.dir/src/core_dump_init.c.obj.d
+++
@@ -1,3 +0,0 @@
-esp-idf/espcoredump/CMakeFiles/__idf_espcoredump.dir/src/core_dump_init.c.obj: \
- /home/ramin/ESP/esp-idf/components/espcoredump/src/core_dump_init.c \
- /home/ramin/Documents/learningCodeForESP32/example1/build/config/sdkconfig.h
--- example1/build/esp-idf/espcoredump/CMakeFiles/__idf_espcoredump.dir/src/core_dump_sha.c.obj.d
+++
@@ -1,3 +0,0 @@
-esp-idf/espcoredump/CMakeFiles/__idf_espcoredump.dir/src/core_dump_sha.c.obj: \
- /home/ramin/ESP/esp-idf/components/espcoredump/src/core_dump_sha.c \
- /home/ramin/Documents/learningCodeForESP32/example1/build/config/sdkconfig.h
